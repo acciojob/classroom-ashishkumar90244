72eba2364ec42f2767b3a032af6c8697119519e0
ashishkumar90244 <ashish.kumarcs0101@gmail.com>
2024-04-05 01:37:11 +0530

    modified StudentRepository

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -18,41 +18,58 @@ public class StudentRepository {
     }
 
     public void saveStudent(Student student){
+    	studentMap.put(student.getName(), student);
         // your code goes here
     }
 
     public void saveTeacher(Teacher teacher){
+    	teacherMap.put(teacher.getName(), teacher);
         // your code goes here
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
+        	if(!teacherStudentMapping.containsKey(teacher)) {
+        		teacherStudentMapping.put(teacher, new LinkedList<>());
+        	}
+        	teacherStudentMapping.get(teacher).add(student);
             // your code goes here
         }
     }
 
     public Student findStudent(String student){
+    	
+    	return studentMap.get(student);
         // your code goes here
     }
 
     public Teacher findTeacher(String teacher){
+    	
+    	return teacherMap.get(teacher);
         // your code goes here
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
+    	
+    	return teacherStudentMapping.get(teacher);
         // your code goes here
         // find student list corresponding to a teacher
     }
 
     public List<String> findAllStudents(){
+    	return new LinkedList<>(studentMap.keySet());
         // your code goes here
     }
 
     public void deleteTeacher(String teacher){
+    	teacherMap.remove(teacher);
+    	teacherStudentMapping.remove(teacher);
         // your code goes here
     }
 
     public void deleteAllTeachers(){
+    	teacherMap.clear();
+    	teacherStudentMapping.clear();
         // your code goes here
     }
 }
